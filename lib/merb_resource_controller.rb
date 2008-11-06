0396f983a27bdbce6e17da6d6f3342b3236cdb19
Martin Gamsjaeger <gamsnjaga@gmail.com>
2008-11-06 04:24:11 +0100

    Added support for dm identity map and action timeouts.

--- lib/merb_resource_controller.rb
+++ lib/merb_resource_controller.rb
@@ -1,9 +1,10 @@
 # make sure we're running inside Merb
 if defined?(Merb::Plugins)
 
-  # Merb gives you a Merb::Plugins.config hash...feel free to put your stuff in your piece of it
+  # Merb gave me a Merb::Plugins.config hash
+  # i felt free to put my stuff in my piece of it
   Merb::Plugins.config[:merb_resource_controller] = {
-    :orm => :datamapper
+    :identity_map => true
   }
   
   Merb::BootLoader.before_app_loads do
@@ -12,6 +13,9 @@ if defined?(Merb::Plugins)
     require DIR / 'resource_proxy'
     require DIR / 'actions'
     require DIR / 'resource_controller'
+    if Merb::Plugins.config[:merb_resource_controller][:identity_map]
+      require DIR / 'identity_map_support'
+    end
   end
   
   Merb::BootLoader.after_app_loads do
--- spec/integration/app/controllers/application.rb
+++ spec/integration/app/controllers/application.rb
@@ -1,3 +1,5 @@
 class Application < Merb::Controller
   extend Merb::ResourceController::Mixin::ClassMethods
+  extend Merb::ResourceController::DM::IdentityMapSupport
+  enable_identity_map :action_timeout => 1
 end
